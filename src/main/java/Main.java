b43f48e520863b7f65fa89a7377f949707f8b151
Nikita Belov <52581468+nikitkagood@users.noreply.github.com>
2026-09-07 23:22:44 +0300

    Architecture update

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,63 +1,94 @@
+import java.nio.file.Paths;
+import java.util.ArrayList;
 import java.util.Scanner;
-class Main {
-    private static void runSortingProcess(Scanner scanner) {
-        // Шаг 1: Выбор заполнения
-        System.out.println("\n--- Выберите способ заполнения массива ---");
-        System.out.println("1. Случайная генерация (рандом)");
-        System.out.println("2. Ручной ввод с клавиатуры");
-        System.out.println("3. Загрузка из файла");
-        System.out.print("Ваш выбор: ");
-        int fillChoice = scanner.nextInt();
-        scanner.nextLine();
+import java.util.ArrayList;
 
-        // Запрашиваем длину (для рандома и ручного ввода, для файла можно игнорировать)
-            System.out.print("Введите длину массива: ");
-            int length = scanner.nextInt();
-            scanner.nextLine();
-        switch (fillChoice) {
-            case 1 -> {
-                System.out.println("Генерация случайных данных...");
+import Car.*;
+import IO.*;
+import Sorter.*;
+
+/*private static void runSortingProcess(Scanner scanner) {
+    // Шаг 1: Выбор заполнения
+    System.out.println("\n--- Выберите способ заполнения массива ---");
+    System.out.println("1. Случайная генерация (рандом)");
+    System.out.println("2. Ручной ввод с клавиатуры");
+    System.out.println("3. Загрузка из файла");
+    System.out.print("Ваш выбор: ");
+    int fillChoice = scanner.nextInt();
+    scanner.nextLine();
+
+    // Запрашиваем длину (для рандома и ручного ввода, для файла можно игнорировать)
+    System.out.print("Введите длину массива: ");
+    int length = scanner.nextInt();
+    scanner.nextLine();
+    switch (fillChoice) {
+        case 1 -> {
+            System.out.println("Генерация случайных данных...");
 //                array = RandomFiller.fill(length);
-            }
-            case 2 -> {
-                System.out.println("Ручной ввод данных:");
+        }
+        case 2 -> {
+            System.out.println("Ручной ввод данных:");
 //                array = ManualFiller.fill(length, scanner);
-            }
-            case 3 -> {
-                System.out.print("Введите путь к файлу (например, data.txt): ");
-                String filePath = scanner.nextLine();
+        }
+        case 3 -> {
+            System.out.print("Введите путь к файлу (например, data.txt): ");
+            String filePath = scanner.nextLine();
 //                array = FileFiller.fill(filePath);
-            }
-            default -> {
-                System.out.println("Неверный выбор. Возврат в главное меню.");
-                return;
-            }
+        }
+        default -> {
+            System.out.println("Неверный выбор. Возврат в главное меню.");
+            return;
+        }
 //            if (array == null || array.length == 0) {
 //                System.out.println("Не удалось заполнить массив.");
 //                return;
 //            }
+    }*/
 
-            // Вызываем выбор поля и сортировку...
-//            chooseSorting(scanner, array);
-        }
-    }
-
+class Main {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
+
         while (true) {
-            System.out.println("=========================================\n" +
-                    "         ПРИЛОЖЕНИЕ СОРТИРОВКИ \n" +
-                    "=========================================");
-            System.out.println("1. Заполнить массив и выполнить сортировку");
-            System.out.println("2. Выход из программы");
-            System.out.print("Выберите действие (1 или 2): ");
+            ConsoleIO.printMainMenu();
+
+            //TODO: валидировать, мб заменить на метод
             int mainChoice = scanner.nextInt();
             scanner.nextLine();
+
             switch (mainChoice) {
                 case 1:
-                    runSortingProcess(scanner);
+                    ConsoleIO.printFillOptions();
+
+                    //TODO: валидировать, мб заменить на метод
+                    int fillChoice = scanner.nextInt();
+                    scanner.nextLine();
+
+                    ConsoleIO.printFillSelection(fillChoice);
+
+                    switch (fillChoice) {
+                        case 1 -> {
+
+                        }
+                        case 2 -> {
+
+                        }
+                        case 3 -> {
+                            //TODO: валидировать
+                            String filePath = scanner.nextLine();
+
+                            ArrayList<Car> cars = new ArrayList<>();
+
+                            CarParser.read(Paths.get(filePath), cars, true);
+                        }
+                    }
+
+                    ArrayList<Car> carList = new ArrayList<>();
+
+                    Sorter.sort(carList);
+
                 case 2: {
-                    System.out.println("Программа завершена. До свидания!");
+                    ConsoleIO.printExit();
                     scanner.close();
                     return;
                 }
